d6dba10a24fbe1e7b9d7f681b2af451c75b1a7d8
Pharaoh <pharaoh.masimo@gmail.com>
2024-05-29 23:00:39 +0200

    front end, alan ai search by source

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { useEffect } from "react";                  // useEffect so our alan Btn is initialized once our platform runs
+import React, { useState, useEffect } from "react";                  // useEffect so our alan Btn is initialized once our platform runs
 import alanBtn from '@alan-ai/alan-sdk-web';               //importing the alan ai 
+import NewsCards from "./components/NewsCards/NewsCards";
 
+import useStyles from './styles.js';
 
 const alanKey = '3bdbf3c51a1dfe6597f5c89bf1e764922e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+    const [newsArticles, setNewsArticles] = useState([]);
+    const classes = useStyles();
 
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({ command }) => {
-                if(command === 'testCommand') {
-                    alert('This code was executed');
+            onCommand: ({ command, articles }) => {
+                if(command === 'newHeadlines') {
+                    setNewsArticles( articles ); 
                 }
             }
         })
@@ -19,7 +23,10 @@ const App = () => {
    
     return (
         <div>
-            <h1>Alan AI News Application</h1>
+            <div className={classes.logoContainer}>
+                <img src="https://assets-global.website-files.com/64ec3fc5bb945b48c0a37b1c/6513eeb6c864c1282f0ea7d9_Customization_benefit1.webp" className={classes.alanLogo} alt="alan logo" />
+            </div>
+            <NewsCards articles={newsArticles}  />
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
 import React from 'react';
-import REACTDOM from 'react-dom';
-
+import ReactDOM from 'react-dom/client';
 import './index.css';
-
 import App from './App';
 
-REACTDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
