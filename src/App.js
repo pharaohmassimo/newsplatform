1278b62ffc969c1633963ec9eeef89f5b5ece392
Pharaoh <pharaoh.masimo@gmail.com>
2024-05-30 08:52:13 +0200

    added the blue bottom border indicating navigation

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ const alanKey = '3bdbf3c51a1dfe6597f5c89bf1e764922e956eca572e1d8b807a3e2338fdd0d
 
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([]);
-    const classes = useStyles();
+    const [activeArticle, setActiveArticle] = useState(-1);            //set active article
+    const classes = useStyles();                            //
 
     useEffect(() => {
         alanBtn({
@@ -16,17 +17,20 @@ const App = () => {
             onCommand: ({ command, articles }) => {
                 if(command === 'newHeadlines') {
                     setNewsArticles( articles ); 
+                    setActiveArticle(-1);
+                } else if(command === 'highlight') {
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);                 //using previous state to change the current state - call it as callback function and simply increment it
                 }
             }
         })
-    }, [])                                                                          //useeffect takes two components - callback function and a dependency array                         
+    }, [])                                                                                          //useeffect takes two components - callback function and a dependency array                         
    
     return (
         <div>
             <div className={classes.logoContainer}>
                 <img src="https://assets-global.website-files.com/64ec3fc5bb945b48c0a37b1c/6513eeb6c864c1282f0ea7d9_Customization_benefit1.webp" className={classes.alanLogo} alt="alan logo" />
             </div>
-            <NewsCards articles={newsArticles}  />
+            <NewsCards articles={newsArticles} activeArticle={activeArticle} />
         </div>
     );
 }
--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -12,7 +12,7 @@ const infoCards = [
     { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
   ];
 
-const NewsCards = ({ articles }) => {
+const NewsCards = ({ articles, activeArticle }) => {                 // destructuring the activeArticle from the props
     const classes = useStyles();
 
     if(!articles.length){
@@ -40,7 +40,7 @@ const NewsCards = ({ articles }) => {
             <Grid className={classes.container}  container alignItems="stretch" spacing={3}>
                 {articles.map((article, i) => (
                     <Grid item xs={12} sm={6} md={4} lg={3} style={ {display : 'flex'} }>           
-                         <NewsCard article={article} i={i}/>                                     
+                         <NewsCard article={article} activeArticle={activeArticle} i={i}/>                                     
                     </Grid>
                 ))}
 
